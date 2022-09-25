3d0fe2d09102bda30cb625d15a503b6f12a1374f
Alexandra Shestakova <alexandra.shestakova@macys.com>
2022-09-25 18:28:50 +0200

    added redux and its logic

---
+++ src/store/cart-slice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
